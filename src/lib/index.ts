dd9b10ce26c8358a98f8a0ca04f5b949eeeb603e
Myrmod <gallewebsites@gmail.com>
2022-01-02 12:31:21 +0100

    update entry in package.json

---
+++ src/lib/index.ts
@@ -0,0 +1,53 @@
+export { default as Canvas } from './components/Canvas/index.svelte'
+
+// Cameras
+export { default as ArcFollowCamera } from './components/Cameras/ArcFollowCamera.svelte'
+export { default as ArcRotateCamera } from './components/Cameras/ArcRotateCamera.svelte'
+export { default as FlyCamera } from './components/Cameras/FlyCamera.svelte'
+export { default as FollowCamera } from './components/Cameras/FollowCamera.svelte'
+export { default as FreeCamera } from './components/Cameras/FreeCamera.svelte'
+
+// Lights
+export { default as DirectionalLight } from './components/Lights/DirectionalLight.svelte'
+export { default as HemisphericLight } from './components/Lights/HemisphericLight.svelte'
+export { default as PointLight } from './components/Lights/PointLight.svelte'
+export { default as SpotLight } from './components/Lights/SpotLight.svelte'
+
+// Objects
+export { default as Box } from './components/Objects/Box.svelte'
+export { default as Capsule } from './components/Objects/Capsule.svelte'
+export { default as CustomSingle } from './components/Objects/CustomSingle.svelte'
+export { default as Cylinder } from './components/Objects/Cylinder.svelte'
+export { default as DashedLines } from './components/Objects/DashedLines.svelte'
+export { default as Decal } from './components/Objects/Decal.svelte'
+export { default as Disc } from './components/Objects/Disc.svelte'
+export { default as Ground } from './components/Objects/Ground.svelte'
+export { default as GroundFromHeightMap } from './components/Objects/GroundFromHeightMap.svelte'
+export { default as IcoSphere } from './components/Objects/IcoSphere.svelte'
+export { default as Lathe } from './components/Objects/Lathe.svelte'
+export { default as LineSystem } from './components/Objects/LineSystem.svelte'
+export { default as Lines } from './components/Objects/Lines.svelte'
+export { default as Polygon } from './components/Objects/Polygon.svelte'
+export { default as Polyhedron } from './components/Objects/Polyhedron.svelte'
+export { default as Ribbon } from './components/Objects/Ribbon.svelte'
+export { default as Sphere } from './components/Objects/Sphere.svelte'
+export { default as TiledBox } from './components/Objects/TiledBox.svelte'
+export { default as TiledGround } from './components/Objects/TiledGround.svelte'
+export { default as TiledPlane } from './components/Objects/TiledPlane.svelte'
+export { default as Torus } from './components/Objects/Torus.svelte'
+export { default as TorusKnot } from './components/Objects/TorusKnot.svelte'
+export { default as Tube } from './components/Objects/Tube.svelte'
+
+// Materials
+export { default as StandardMaterial } from './components/Materials/StandardMaterial/index.svelte'
+
+// Textures
+export { default as AmbientTexture } from './components/Textures/AmbientTexture.svelte'
+export { default as BumpTexture } from './components/Textures/BumpTexture.svelte'
+export { default as DiffuseTexture } from './components/Textures/DiffuseTexture.svelte'
+export { default as EmissiveTexture } from './components/Textures/EmissiveTexture.svelte'
+export { default as LightmapTexture } from './components/Textures/LightmapTexture.svelte'
+export { default as OpacityTexture } from './components/Textures/OpacityTexture.svelte'
+export { default as ReflectionTexture } from './components/Textures/ReflectionTexture.svelte'
+export { default as RefractionTexture } from './components/Textures/RefractionTexture.svelte'
+export { default as SpecularTexture } from './components/Textures/SpecularTexture.svelte'
