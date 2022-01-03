07e49fb1be3463d7a3f0fbc93d828e694c9cb064
Myrmod <gallewebsites@gmail.com>
2022-01-03 17:55:24 +0100

    improve lighting of custom meshes scene

--- src/routes/examples/custom-meshes.svelte
+++ src/routes/examples/custom-meshes.svelte
@@ -22,11 +22,12 @@
     position={new BABYLON.Vector3(2, 1.75, -8)}
     target={new BABYLON.Vector3(0, 1.75, 0)}
   />
-  <DirectionalLight shadowEnabled direction={new BABYLON.Vector3(0, -1, 0)} intensity={0.5} />
-  <HemisphericLight shadowEnabled intensity={0.25} />
+  <DirectionalLight shadowEnabled direction={new BABYLON.Vector3(0.5, -1, 0.5)} intensity={3} />
+  <HemisphericLight shadowEnabled intensity={0.1} />
   <Custom
     name="campfire"
     fileName="assets/models/campfire.glb"
     scaling={new BABYLON.Vector3(1.5, 1.5, 1.5)}
+    receiveShadows
   />
 </Canvas>
