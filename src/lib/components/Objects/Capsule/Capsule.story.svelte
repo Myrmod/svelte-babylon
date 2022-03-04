3d5b3607191eacbcbb2fc5d08f9b633dd4a62e95
Pascal Galle <p.galle@vke.io>
2022-03-04 09:22:08 +0100

    Add basic documentation for most objects

---
+++ src/lib/components/Objects/Capsule/Capsule.story.svelte
@@ -0,0 +1,53 @@
+<script lang="ts" context="module">
+  import {
+    ArcRotateCamera,
+    Canvas,
+    Capsule,
+    DirectionalLight,
+    Ground,
+    HemisphericLight,
+  } from '$lib'
+  import type { PageMeta } from '@vitebook/client'
+  import * as BABYLON from 'babylonjs'
+
+  export const __pageMeta: PageMeta = {
+    title: 'Cylinder',
+    description: '',
+  }
+</script>
+
+<script lang="ts">
+  const objectPosition = new BABYLON.Vector3(0, 3, 0)
+
+  let object: {
+    self: BABYLON.Mesh | BABYLON.AbstractMesh
+  }
+
+  let shadowObjects: Array<typeof object['self']>
+  $: {
+    const temp: typeof shadowObjects = []
+    if (object?.self) {
+      temp.push(object.self)
+    }
+    shadowObjects = temp
+  }
+</script>
+
+<Canvas
+  antialiasing={true}
+  engineOptions={{
+    preserveDrawingBuffer: true,
+    stencil: true,
+  }}
+>
+  <HemisphericLight intensity={0.5} />
+  <DirectionalLight
+    intensity={0.25}
+    direction={new BABYLON.Vector3(-10, -20, -10)}
+    position={new BABYLON.Vector3(2, 6, 2)}
+    castShadowOf={shadowObjects}
+  />
+  <ArcRotateCamera target={objectPosition} />
+  <Capsule y={3} bind:object />
+  <Ground options={{ width: 6, height: 6, subdivisions: 2 }} receiveShadows y={1} />
+</Canvas>
---
+++ src/lib/components/Objects/Polyhedron/Polyhedron.story.svelte
@@ -0,0 +1,53 @@
+<script lang="ts" context="module">
+  import {
+    ArcRotateCamera,
+    Canvas,
+    DirectionalLight,
+    Ground,
+    HemisphericLight,
+    Polyhedron,
+  } from '$lib'
+  import type { PageMeta } from '@vitebook/client'
+  import * as BABYLON from 'babylonjs'
+
+  export const __pageMeta: PageMeta = {
+    title: 'Polyhedron',
+    description: '',
+  }
+</script>
+
+<script lang="ts">
+  const objectPosition = new BABYLON.Vector3(0, 3, 0)
+
+  let object: {
+    self: BABYLON.Mesh | BABYLON.AbstractMesh
+  }
+
+  let shadowObjects: Array<typeof object['self']>
+  $: {
+    const temp: typeof shadowObjects = []
+    if (object?.self) {
+      temp.push(object.self)
+    }
+    shadowObjects = temp
+  }
+</script>
+
+<Canvas
+  antialiasing={true}
+  engineOptions={{
+    preserveDrawingBuffer: true,
+    stencil: true,
+  }}
+>
+  <HemisphericLight intensity={0.5} />
+  <DirectionalLight
+    intensity={0.25}
+    direction={new BABYLON.Vector3(-10, -20, -10)}
+    position={new BABYLON.Vector3(2, 6, 2)}
+    castShadowOf={shadowObjects}
+  />
+  <ArcRotateCamera target={objectPosition} />
+  <Polyhedron y={3} bind:object />
+  <Ground options={{ width: 6, height: 6, subdivisions: 2 }} receiveShadows y={1} />
+</Canvas>
