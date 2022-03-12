288d24811e2a0a8c8af15e55930d8e7f84036d12
Myrmod <gallewebsites@gmail.com>
2022-03-12 11:54:03 +0100

    add skybox example

---
+++ src/lib/components/Objects/Skybox/Skybox.story.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+  import { ArcRotateCamera, Skybox, Canvas, HemisphericLight } from '$lib'
+  import type { PageMeta } from '@vitebook/client'
+
+  export const __pageMeta: PageMeta = {
+    title: 'Skybox',
+    description: '',
+  }
+</script>
+
+<Canvas
+  antialiasing={true}
+  engineOptions={{
+    preserveDrawingBuffer: true,
+    stencil: true,
+  }}
+>
+  <HemisphericLight />
+  <ArcRotateCamera />
+  <Skybox rootUrl="/assets/textures/skybox/sky" />
+</Canvas>
