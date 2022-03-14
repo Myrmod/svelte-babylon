64019b0cd3e14a65d23a5591171c37079106b29c
Pascal Galle <p.galle@vke.io>
2022-03-14 10:33:01 +0100

    update documentation and add a getting started page

--- .vitebook/config.js
+++ .vitebook/config.js
@@ -1,22 +1,26 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { clientPlugin, defineConfig } from '@vitebook/client/node'
+import { shikiMarkdownPlugin } from '@vitebook/markdown-shiki/node'
+import { svelteMarkdownPlugin } from '@vitebook/markdown-svelte/node'
 import { defaultThemePlugin } from '@vitebook/theme-default/node'
 import preprocess from 'svelte-preprocess'
 
 export default defineConfig({
-  include: ['src/**/*.story.svelte'],
+  include: ['src/**/*.md', 'src/**/*.story.svelte'],
   alias: {
     $app: '/node_modules/@sveltejs/kit/assets/app',
     $lib: '/src/lib',
   },
   plugins: [
+    svelteMarkdownPlugin(),
+    shikiMarkdownPlugin(),
     clientPlugin({ appFile: 'App.svelte' }),
     defaultThemePlugin(),
     svelte({
       compilerOptions: {
         hydratable: true,
       },
-      extensions: ['.svelte'],
+      extensions: ['.svelte', '.md'],
       // Consult https://github.com/sveltejs/svelte-preprocess for more information
       // about preprocessors.
       preprocess: preprocess(),
@@ -34,6 +38,31 @@ export default defineConfig({
     theme: {
       darkMode: {
         enabled: true,
+        buttonAriaLabel: 'Toggle dark mode',
+      },
+      remoteGitRepo: {
+        url: 'https://github.com/Myrmod/svelte-babylon',
+      },
+      socials: {
+        twitter: 'https://twitter.com/myrmod',
+      },
+      homePage: {
+        heroText: 'Svelte Babylon',
+        primaryActionText: 'Get Started',
+        primaryActionLink: '/getting-started.html',
+        // secondaryActionText: 'Documentation',
+        // secondaryActionLink: '/examples/rotatecameratoclickedface.html',
+        // features: [
+        //   {
+        //     title: 'Instant Server Start',
+        //     body: 'On demand file serving over native ESM, no bundling required!',
+        //   },
+        // ],
+        footer: `MIT Licensed (modified) | Copyright © ${new Date().getFullYear()}`,
+      },
+      sidebar: {
+        toggleAriaLabel: 'Toggle sidebar menu',
+        style: 'docs',
       },
     },
   },
