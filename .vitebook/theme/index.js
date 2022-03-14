2d91875a652c79c33d9dc7c7e7cfdad65f7e5678
Pascal Galle <p.galle@vke.io>
2022-03-14 12:36:29 +0100

    add privacy page for documentation website

--- .vitebook/theme/index.js
+++ .vitebook/theme/index.js
@@ -1,3 +1,7 @@
-import Theme from '@vitebook/theme-default';
+import Theme from '@vitebook/theme-default'
+import Layout from './Layout.svelte'
 
-export default Theme;
+export default {
+  ...Theme,
+  Layout,
+}
