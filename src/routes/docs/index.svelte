a46f6d483c1941158ae4fd4342509468f4e79b0d
Myrmod <pascal.galle@gmx.de>
2022-04-18 17:38:51 +0200

    start documentation

--- src/routes/docs/index.svelte
+++ src/routes/docs/index.svelte
@@ -5,7 +5,7 @@
   export function load() {
     return {
       status: 307,
-      redirect: '/docs/first-category/first-page',
+      redirect: '/docs/introduction/getting-started',
     }
   }
 </script>
