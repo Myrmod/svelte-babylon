d3d1c58c567bfd7b359c7b3039b7c6545d16d488
Pascal Galle <p.galle@vke.io>
2022-04-28 12:50:59 +0200

    add component props type helper

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -51,3 +51,8 @@ export default interface RootContext {
     }
   }
 }
+
+// This returns all the component's properties as a Partial: { prop1?: string, ... } use it like ComponentProperties<MyComponent>
+export type ComponentProperties<T extends { $set: (...args: any) => any }> = NonNullable<
+  Parameters<T['$set']>[0]
+>
