05e4d2598112fe0ab3dfb2f0abaf4ed2aed1fd96
Pascal Galle <p.galle@vke.io>
2022-05-05 12:14:54 +0200

    update shuffle story

--- src/examples/Shuffle.story.svelte
+++ src/examples/Shuffle.story.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import type RootContext from '$lib/types'
   import { Animation } from '@babylonjs/core/Animations/animation.js'
   import { Color3 } from '@babylonjs/core/Maths/math.color'
   import { Vector3 } from '@babylonjs/core/Maths/math.vector'
+  import type { Mesh } from '@babylonjs/core/Meshes/mesh'
+  import type { Scene as BScene } from '@babylonjs/core/scene.js'
   import { ArcRotateCamera } from 'svelte-babylon'
   import Canvas from 'svelte-babylon/components/Canvas/index.svelte'
   import Button from 'svelte-babylon/components/GUI/Button/index.svelte'
@@ -11,10 +12,12 @@
   import HemisphericLight from 'svelte-babylon/components/Lights/HemisphericLight/index.svelte'
   import StandardMaterial from 'svelte-babylon/components/Materials/StandardMaterial/index.svelte'
   import Box from 'svelte-babylon/components/Objects/Box/index.svelte'
-  import type { Writable } from 'svelte/store'
+  import Scene from 'svelte-babylon/components/Scene/index.svelte'
+  import { get, type Writable } from 'svelte/store'
 
-  let root: Writable<RootContext>
-  let boxes = [[], [], []]
+  let scene: Writable<BScene>
+
+  let boxes: Array<Array<Writable<Mesh>>> = [[], [], []]
   const matrix = [
     [
       { x: 0, y: 0 },
@@ -34,9 +37,10 @@
   ]
   let newPositionMatrix: typeof matrix
 
-  $: if ($root && newPositionMatrix) {
+  $: if ($scene && newPositionMatrix) {
     boxes.forEach((row, i) => {
-      row.forEach((box, j) => {
+      row.forEach((boxStore, j) => {
+        const box = get(boxStore)
         if (!box) return
 
         const anim = new Animation(
@@ -49,16 +53,16 @@
         anim.setKeys([
           {
             frame: 0,
-            value: new Vector3(box.self.position.x, box.self.position.y, 0),
+            value: new Vector3(box.position.x, box.position.y, 0),
           },
           {
             frame: 100,
             value: new Vector3(newPositionMatrix[i][j].x, newPositionMatrix[i][j].y, 0),
           },
         ])
-        box.self.animations.push(anim)
+        box.animations.push(anim)
 
-        $root.scene.beginAnimation(box.self, 0, 100, false, 1)
+        $scene.beginAnimation(box, 0, 100, false, 1)
       })
     })
   }
@@ -93,31 +97,31 @@
     preserveDrawingBuffer: true,
     stencil: true,
   }}
-  bind:root
-  animationsEnabled
 >
-  <HemisphericLight intensity={0.5} />
-  <DirectionalLight
-    intensity={0.25}
-    direction={new Vector3(-10, -20, -10)}
-    position={new Vector3(2, 6, 2)}
-  />
-  <ArcRotateCamera target={new Vector3(1, 1, 0)} radius={10} />
-  {#each matrix as row, x}
-    {#each row as item, y}
-      <Box {...item} bind:object={boxes[x][y]}>
-        <StandardMaterial diffuseColor={Color3.Random()} />
-      </Box>
-    {/each}
-  {/each}
-  <GUI>
-    <Button
-      text="shuffle"
-      onPointerUp={() => {
-        newPositionMatrix = shuffle(matrix)
-        boxes = boxes
-      }}
-      top={-240}
+  <Scene animationsEnabled bind:scene>
+    <HemisphericLight intensity={0.5} />
+    <DirectionalLight
+      intensity={0.25}
+      direction={new Vector3(-10, -20, -10)}
+      position={new Vector3(2, 6, 2)}
     />
-  </GUI>
+    <ArcRotateCamera target={new Vector3(1, 1, 0)} radius={10} />
+    {#each matrix as row, x}
+      {#each row as item, y}
+        <Box {...item} bind:object={boxes[x][y]}>
+          <StandardMaterial diffuseColor={Color3.Random()} />
+        </Box>
+      {/each}
+    {/each}
+    <GUI>
+      <Button
+        text="shuffle"
+        onPointerUp={() => {
+          newPositionMatrix = shuffle(matrix)
+          boxes = boxes
+        }}
+        top={-240}
+      />
+    </GUI>
+  </Scene>
 </Canvas>
