bc361b15de58877aa93b4535a1de529f742adbd9
Myrmod <pascal.galle@gmx.de>
2022-05-06 20:43:00 +0200

    create first implementation of xr scene

--- src/lib/components/XRScene/index.svelte
+++ src/lib/components/XRScene/index.svelte
@@ -3,10 +3,10 @@ An implementation of https://doc.babylonjs.com/divingDeeper/webXR/webXRSessionMa
  -->
 <script lang="ts">
   import { createReactiveContext } from '$lib/utils/createReactiveContext'
-  import {
-    WebXRManagedOutputCanvasOptions,
-    WebXRSessionManager,
-    type WebXRRenderTarget,
+  import type {
+    AbstractMesh,
+    WebXRDefaultExperience,
+    WebXRDefaultExperienceOptions,
   } from '@babylonjs/core'
   import type { Engine } from '@babylonjs/core/Engines/engine.js'
   import { Color3, Color4 } from '@babylonjs/core/Maths/math.color'
@@ -15,7 +15,7 @@ An implementation of https://doc.babylonjs.com/divingDeeper/webXR/webXRSessionMa
   import type { CannonJSPlugin as CannonJSPluginType } from '@babylonjs/core/Physics/Plugins/cannonJSPlugin'
   import type { OimoJSPlugin as OimoJSPluginType } from '@babylonjs/core/Physics/Plugins/oimoJSPlugin'
   import { Scene, type SceneOptions } from '@babylonjs/core/scene.js'
-  import { getContext, onDestroy, onMount } from 'svelte'
+  import { getContext, onMount } from 'svelte'
   import type { Writable } from 'svelte/store'
 
   // settings
@@ -42,17 +42,9 @@ An implementation of https://doc.babylonjs.com/divingDeeper/webXR/webXRSessionMa
     }
   })
   $scene.physicsEnabled = false
-  export let sessionManager = createReactiveContext(
-    'sessionManager',
-    new WebXRSessionManager($scene),
-  )
-  export let sessionMode: XRSessionMode = 'immersive-vr'
-  export let referenceSpace = createReactiveContext<XRReferenceSpace>('referenceSpace', undefined)
-  export let referenceSpaceType: XRReferenceSpaceType = 'viewer'
-  export let renderTargetOptions: WebXRManagedOutputCanvasOptions = undefined
-  export let renderTarget = createReactiveContext<WebXRRenderTarget>('renderTarget', undefined)
-  export let xrWebGLLayer: XRWebGLLayer = undefined
 
+  export let xrHelper = createReactiveContext<WebXRDefaultExperience>('xrHelper', undefined)
+  export let options: WebXRDefaultExperienceOptions = undefined
   onMount(async () => {
     try {
       // polyfill
@@ -75,25 +67,14 @@ An implementation of https://doc.babylonjs.com/divingDeeper/webXR/webXRSessionMa
         }
       })
 
-      if (!(await $sessionManager.isSessionSupportedAsync(sessionMode))) {
-        throw new Error(`"${sessionMode}" is not supported on your device`)
+      await import('@babylonjs/core/Materials/Node/Blocks')
+      await import('@babylonjs/core/Helpers/sceneHelpers.js')
+
+      $xrHelper = await $scene.createDefaultXRExperienceAsync(options)
+
+      if ($xrHelper.baseExperience) {
+        await import('@babylonjs/loaders/glTF')
       }
-
-      await $sessionManager.initializeAsync()
-      await $sessionManager.initializeSessionAsync(sessionMode)
-
-      $referenceSpace = await $sessionManager.setReferenceSpaceTypeAsync(referenceSpaceType)
-      $renderTarget = $sessionManager.getWebXRRenderTarget(renderTargetOptions)
-      xrWebGLLayer = await $renderTarget.initializeXRLayerAsync($sessionManager.session)
-
-      // await import('@babylonjs/core/Materials/Node/Blocks')
-      // await import('@babylonjs/core/Helpers/sceneHelpers.js')
-
-      // xrHelper = await $scene.createDefaultXRExperienceAsync()
-
-      // if (xrHelper.baseExperience) {
-      //   await import('@babylonjs/loaders')
-      // }
 
       if (animationsEnabled) {
         await import('@babylonjs/core/Animations/animatable')
@@ -110,83 +91,70 @@ An implementation of https://doc.babylonjs.com/divingDeeper/webXR/webXRSessionMa
     }
   })
 
-  onDestroy(() => {
-    $renderTarget.dispose()
-    $sessionManager.dispose()
-  })
-
-  export let stop = false
-  $: if ($sessionManager && !stop) {
-    $sessionManager.runXRRenderLoop()
-  }
-  $: if ($sessionManager && stop) {
-    $sessionManager.exitXRAsync()
-  }
-
   // handle floor meshes
-  // export let floorMeshes: Array<AbstractMesh> = undefined
-  // export const activeFloorMeshes: Array<AbstractMesh> = []
-  // let floorMeshesToRemove: Array<AbstractMesh> = []
-  // $: if (xrHelper?.baseExperience) {
-  //   floorMeshes?.forEach(floorMesh => {
-  //     if (activeFloorMeshes.includes(floorMesh)) {
-  //       return
-  //     }
-  //     activeFloorMeshes.push(floorMesh)
-  //     xrHelper.teleportation.addFloorMesh(floorMesh)
-  //   })
-  //   floorMeshesToRemove = activeFloorMeshes.filter(
-  //     activeFloorMesh => !floorMeshes?.includes(activeFloorMesh),
-  //   )
-  // }
-  // $: if (xrHelper?.baseExperience && floorMeshesToRemove.length) {
-  //   floorMeshesToRemove.forEach(floorMesh => xrHelper.teleportation.removeFloorMesh(floorMesh))
-  //   floorMeshesToRemove = []
-  // }
+  export let floorMeshes: Array<AbstractMesh> = undefined
+  export const activeFloorMeshes: Array<AbstractMesh> = []
+  let floorMeshesToRemove: Array<AbstractMesh> = []
+  $: if ($xrHelper?.baseExperience) {
+    floorMeshes?.forEach(floorMesh => {
+      if (activeFloorMeshes.includes(floorMesh)) {
+        return
+      }
+      activeFloorMeshes.push(floorMesh)
+      $xrHelper.teleportation.addFloorMesh(floorMesh)
+    })
+    floorMeshesToRemove = activeFloorMeshes.filter(
+      activeFloorMesh => !floorMeshes?.includes(activeFloorMesh),
+    )
+  }
+  $: if ($xrHelper?.baseExperience && floorMeshesToRemove.length) {
+    floorMeshesToRemove.forEach(floorMesh => $xrHelper.teleportation.removeFloorMesh(floorMesh))
+    floorMeshesToRemove = []
+  }
 
-  // // handle blocker meshes
-  // export let blockerMeshes: Array<AbstractMesh> = undefined
-  // export const activeBlockerMeshes: Array<AbstractMesh> = []
-  // let blockerMeshesToRemove: Array<AbstractMesh> = []
-  // $: if (xrHelper?.baseExperience) {
-  //   blockerMeshes?.forEach(blockerMesh => {
-  //     if (activeBlockerMeshes.includes(blockerMesh)) {
-  //       return
-  //     }
-  //     activeBlockerMeshes.push(blockerMesh)
-  //     xrHelper.teleportation.addBlockerMesh(blockerMesh)
-  //   })
-  //   blockerMeshesToRemove = activeBlockerMeshes.filter(
-  //     activeBlockerMesh => !blockerMeshes?.includes(activeBlockerMesh),
-  //   )
-  // }
-  // $: if (xrHelper?.baseExperience && blockerMeshesToRemove.length) {
-  //   blockerMeshesToRemove.forEach(blockerMesh =>
-  //     xrHelper.teleportation.removeBlockerMesh(blockerMesh),
-  //   )
-  //   blockerMeshesToRemove = []
-  // }
+  // handle blocker meshes
+  export let blockerMeshes: Array<AbstractMesh> = undefined
+  export const activeBlockerMeshes: Array<AbstractMesh> = []
+  let blockerMeshesToRemove: Array<AbstractMesh> = []
+  $: if ($xrHelper?.baseExperience) {
+    blockerMeshes?.forEach(blockerMesh => {
+      if (activeBlockerMeshes.includes(blockerMesh)) {
+        return
+      }
+      activeBlockerMeshes.push(blockerMesh)
+      $xrHelper.teleportation.addBlockerMesh(blockerMesh)
+    })
+    blockerMeshesToRemove = activeBlockerMeshes.filter(
+      activeBlockerMesh => !blockerMeshes?.includes(activeBlockerMesh),
+    )
+  }
+  $: if ($xrHelper?.baseExperience && blockerMeshesToRemove.length) {
+    blockerMeshesToRemove.forEach(blockerMesh =>
+      $xrHelper.teleportation.removeBlockerMesh(blockerMesh),
+    )
+    blockerMeshesToRemove = []
+  }
 
-  // // handle snap points
-  // export let snapPoints: Array<Vector3> = undefined
-  // export const activeSnapPoints: Array<Vector3> = []
-  // let snapPointsToRemove: Array<Vector3> = []
-  // $: if (xrHelper?.baseExperience) {
-  //   snapPoints?.forEach(snapPoint => {
-  //     if (activeSnapPoints.includes(snapPoint)) {
-  //       return
-  //     }
-  //     activeSnapPoints.push(snapPoint)
-  //     xrHelper.teleportation.addSnapPoint(snapPoint)
-  //   })
-  //   snapPointsToRemove = activeSnapPoints.filter(
-  //     activeBlockerMesh => !snapPoints?.includes(activeBlockerMesh),
-  //   )
-  // }
-  // $: if (xrHelper?.baseExperience && snapPointsToRemove.length) {
-  //   snapPointsToRemove.forEach(snapPoint => xrHelper.teleportation.removeSnapPoint(snapPoint))
-  //   snapPointsToRemove = []
-  // }
+  // handle snap points
+  export let snapPoints: Array<Vector3> = undefined
+  export const activeSnapPoints: Array<Vector3> = []
+  let snapPointsToRemove: Array<Vector3> = []
+  $: if ($xrHelper?.baseExperience) {
+    snapPoints?.forEach(snapPoint => {
+      if (activeSnapPoints.includes(snapPoint)) {
+        return
+      }
+      activeSnapPoints.push(snapPoint)
+      $xrHelper.teleportation.addSnapPoint(snapPoint)
+    })
+    snapPointsToRemove = activeSnapPoints.filter(
+      activeBlockerMesh => !snapPoints?.includes(activeBlockerMesh),
+    )
+  }
+  $: if ($xrHelper?.baseExperience && snapPointsToRemove.length) {
+    snapPointsToRemove.forEach(snapPoint => $xrHelper.teleportation.removeSnapPoint(snapPoint))
+    snapPointsToRemove = []
+  }
 
   // general stuff
   $: if ($scene && $scene.cameras.length) {
