188b31f867793297243080bb1fd7b80fb4240280
Myrmod <pascal.galle@gmx.de>
2022-05-07 09:32:39 +0200

    add basic test for XRScene

--- tests/componentsThrowNoErrorUponInitialization.spec.ts
+++ tests/componentsThrowNoErrorUponInitialization.spec.ts
@@ -183,6 +183,10 @@ test.describe('Components throw no error upon initialization', () => {
       url: '/docs/components/layer',
       component: 'LayerFromBase64StringStory',
     },
+    {
+      name: 'XRScene',
+      url: '/docs/components/xr-ar-v',
+    },
   ].forEach(({ name, url, component }) => {
     test(name, async ({ page }) => {
       page.on('console', message => {
